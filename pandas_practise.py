c974ba9422275a4dc6795669b98c3762d99b4adf
iainr <rixyian@gmail.com>
2025-11-29 20:36:08 +0000

    Add new py file specifically for youtube pandas tutorials, as well as csv file needed for tutorial

---
+++ pandas_practise.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df1 = pd.read_csv("orders.csv") #`.read_csv` is what pd uses for csv files
+print(df1)
+
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'Country': ['USA', 'Canada', 'UK']
+}
+
+df2 = pd.DataFrame(data) #`.dataframe` is what pd uses for dictionaries
+print(data)
+print(df2)
+print(df1)
