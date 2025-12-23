185e9ee0c0cf4df098914c20e3cf9204ef607fe6
iainr <rixyian@gmail.com>
2025-12-23 20:58:36 +0000

    Other stuff for practise prior to laptop death

--- pandas_practise.py
+++ pandas_practise.py
@@ -16,20 +16,37 @@ print(df1)'''
 
 #-----------looking into NN via numpy via youtube Sentdex tutorial series
 
-inputs = [1,2,3,2.5]
+inputs = [1, 2, 3, 2.5]
 
-weights_seen_by_node1 = [0.2,   0.8,    -0.5,   1]
+'''weights_seen_by_node1 = [0.2,   0.8,    -0.5,   1]
 weights_seen_by_node2 = [0.5,   -0.91,  0.26,   -0.5]
-weights_seen_by_node3 = [-0.26, -0.27,  0.17,   0.87]
+weights_seen_by_node3 = [-0.26, -0.27,  0.17,   0.87]'''
 
-bias_on_node1 = 2
+weights_seen_by_nodes = [[0.2,   0.8,    -0.5,   1],
+                         [0.5,   -0.91,  0.26,   -0.5],
+                         [-0.26, -0.27,  0.17,   0.87]]
+bias_on_nodes = [2, 3, 0.5]
+'''bias_on_node1 = 2
 bias_on_node2 = 3
-bias_on_node3 = 0.5
+bias_on_node3 = 0.5'''
+layer_outputs = [] # list where current layer's output is stored
+'''zip() combines 2+ lists, strings, dictionaries, etc into single array,
+where each element in the array is a pair/list of the elements that shared
+the same index in the original arrays'''
+'''for n_weights, n_bias in zip(weights_seen_by_nodes, bias_on_nodes):
+    n_output = 0 #initialise given nwurone (n)'s output
+    for n_input, weight in zip(inputs, n_weights):
+        n_output += n_input
+    n_output += n_bias'''
+what_zip_does = zip(weights_seen_by_nodes, bias_on_nodes)
+print(weights_seen_by_nodes)
+print(bias_on_nodes)
+print(list(what_zip_does))
 
-output = [inputs[0]*weights_seen_by_node1[0] + inputs[1]*weights_seen_by_node1[1] + inputs[2]*weights_seen_by_node1[2] + inputs[3]*weights_seen_by_node1[3] + bias_on_node1,
+'''output = [inputs[0]*weights_seen_by_node1[0] + inputs[1]*weights_seen_by_node1[1] + inputs[2]*weights_seen_by_node1[2] + inputs[3]*weights_seen_by_node1[3] + bias_on_node1,
           inputs[0]*weights_seen_by_node2[0] + inputs[1]*weights_seen_by_node2[1] + inputs[2]*weights_seen_by_node2[2] + inputs[3]*weights_seen_by_node2[3] + bias_on_node2,
-          inputs[0]*weights_seen_by_node3[0] + inputs[1]*weights_seen_by_node3[1] + inputs[2]*weights_seen_by_node3[2] + inputs[3]*weights_seen_by_node3[3] + bias_on_node3]
+          inputs[0]*weights_seen_by_node3[0] + inputs[1]*weights_seen_by_node3[1] + inputs[2]*weights_seen_by_node3[2] + inputs[3]*weights_seen_by_node3[3] + bias_on_node3]'''
 
-print(output)
+#print(output)
 
 #for x in inputs
--- python_catchup.py
+++ python_catchup.py
@@ -1,9 +1,9 @@
-print("hello owlrd")
+'''print("hello owlrd")
 print("1")
 print("2")
 print("3")
 print("4")
-print("5")
+print("5")'''
 
 # single line comments in python are with '#'
 """multiline commnents are with 
@@ -66,9 +66,7 @@ def evaluate_temp(temp):
         message = "Fever!" 
     return message
 
-print("37C = ", evaluate_temp(37))
-print("38C = ", evaluate_temp(38))
-print("39C = ", evaluate_temp(39))
+print("16C = ", evaluate_temp(16),", 38C = ", evaluate_temp(38),", 39C = ", evaluate_temp(39))
 
 #   if else statement syntax----------------------------------------
 def evaluate_temp_with_else(temp):
@@ -135,3 +133,130 @@ print("Maximum:", max(hardcover_sales))
 print("Total books sold in one week:", sum(hardcover_sales))
 print("Average books sold in first 5 days:", sum(hardcover_sales[:5])/5)
 print("Average books sold in last 3 days:", sum(hardcover_sales[-3:])/3)
+
+#python maths in the Kaggle Notebooks tutorial:
+#True div:	a / b = quotient of a & b
+#Floor div:	a // b = quotient of a & b, excl fractional parts
+#modulus:	a % b = in remainder after division of a by b
+
+#True div:
+print("4/2 =", 4/2,", type =",type(4/2))# 2.0
+print("3/2 =", 3/2,", type =",type(3/2))# 1.5
+print("1823/100 =", 1823/100,", type =",type(1823/100),"\n")# 18.23
+
+#floor div:
+print("4//2 =", 4//2,", type =",type(4//2))# 2
+print("3//2 =", 3//2,", type =",type(3//2))# 1
+print("1823//100 =", 1823//100,", type =",type(1823//100),"\n")# 18
+
+#modulus:
+print("4%2 =", 4%2,", type =",type(4%2))# 0
+print("3%2 =", 3%2,", type =",type(3%2))# 1
+print("1823%100 =", 1823%100,", type =",type(1823%100),"\n")# 23
+
+print("ergo, floor div ('//') and modulus ('%') are opposites:\nthe former gives only the number before the decimal,\nwhilst the latter gives only the number after\n")
+
+'''a = [1, 2, 3]
+b = [3, 2, 1]
+c = a # store 'a' in another variable
+print("c =",c)
+a = b # now replace 'a' with smth else
+print("c =",c) # c has remained the same, it doesnt change after 'a' changes
+b = c
+print("a =",a,"\nb =",b)'''
+
+'''	a = [1, 2, 3]
+	b = [3, 2, 1]'''
+'''a, b = [1, 2, 3], [3, 2, 1]
+a, b = b, a
+print("a: ",a,"\nb: ",b,"\n")
+
+print ("5-3//2 =",5-3//2) # is different if u stick brackets in it
+print ("(5-3)//2 =",(5-3)//2)
+print ("5-(3//2) =",5-(3//2),"\n") # but does this still work if the
+# brackets aren't next to an operator ('//' in this case)?
+
+print("8-3*2-1+1 =",8-3*2-1+1)
+print("(8-3)*(2(-1+1)) =",(8-3)*(2-(1+1)))'''
+# ok, not sure what was such a problem here: 
+
+#-----------------learning docstrings & fstrings again:
+#   use docstrings in custom funcs in massive programs to say vital stuff
+def greet(name):
+    '''this should be a docstring, explaining what this func does'''
+    return f"Hello, no.{name}!" # think the only difference is that this
+# cuts down on the number of '""' needed in normal str that uses varibles
+print(greet("234"))
+
+# looks like an fstring can be saved as an entire variable, converting
+# the new stuff into chars since the whole things is considered a `str`
+'''for x in range(4):
+  print(x)
+  #print("Greet is now:", greet(x))
+  combined_str = f"x is now {x}"
+  print(combined_str, type(combined_str))'''
+
+#   however, the saved fstring will only take that variable as it was,
+#   it wont notice any change to the included variable, ie if x becomes 2
+'''x = 1
+combined_str = f"x is now {x}"
+print(combined_str)
+x = 2
+print(combined_str)'''
+
+#------------can also use __doc__ to get the same info as help()
+print(f"via `.__doc__`: {greet.__doc__}")
+print(help(greet)) # apparently `help()` must be the only thing in the call
+# as it displays it in a structured preset format
+
+'''-syntax for docstrings:
+    -it should start with a capital letter & end with a period
+    -first line should be a short desc to summarise
+    -if any more lines, then leave 2nd line blank to separate the summary
+    from bulk of the text below
+    -the lines that follow should be 1+ paragraphs that desc the object's
+    calling conventions, side effects, etc (examples of func calls is nice)'''
+
+def mod_5(x):
+    """Return the remainder of x after dividing by 5"""
+    return x % 5
+
+def mod_7(x):
+    """Return the remainder of x after dividing by 7"""
+    return x % 7
+
+def floor_div_3(x):
+    """Return the remainder of x after dividing by 7"""
+    return x // 3
+
+#help(max) #if 2+ items are highest equally, it picks the first in the order
+
+'''print(
+    'Which number is biggest?',
+    max(100, 51, 14),
+    'Which number has the biggest quotient after being divided by 3?',
+    max(51, 100, 14, key=floor_div_3),
+    sep='\n',
+)'''
+
+'''def is_odd(n):
+    return (n % 2) == 1
+
+print("Is 100 odd?", is_odd(100))
+print("Is -1 odd?", is_odd(-1))'''
+
+def can_run_for_president(age, is_natural_born_citizen):
+    """Can someone of the given age and citizenship status run for president in the US?"""
+    # The US Constitution says you must be a natural born citizen *and* at least 35 years old
+    return is_natural_born_citizen and (age >= 35)
+print(f"19 y/o citizen:{can_run_for_president(19, True)}")
+print(f"55 y/o non-citizen: {can_run_for_president(55, False)}")
+print(f"55 y/o citizen: {can_run_for_president(55, True)}")
+
+#-----------------example of more ddvanced conditional using bools:
+have_umbrella = False
+rain_level = 0
+have_hood = False
+is_workday = False
+
+prepared_for_weather = have_umbrella or ((rain_level < 5) and have_hood) or (not (rain_level > 0 and is_workday))
